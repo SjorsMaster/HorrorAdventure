6e914983984d9653644a593683b94b462bc8d0a7
ShaileshSSC <shaileshchotoe2001@gmail.com>
2019-01-14 11:43:07 +0100

    Basic player movement

---
+++ UnityProject/Assets/Scripts/Shailesh/Managers/InputManagerGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class InputManagerGame : MonoBehaviour
+{
+    string[] keys = File.ReadAllLines("Assets/Resources/keys.txt");
+    GameObject kaas;
+    enum Dir { up, down, left, right };
+    
+    private PlayerControl anotherScript;
+
+    private void Start()
+    {
+        kaas = GameObject.Find("Player");
+        anotherScript = kaas.GetComponent<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(keys[(int)Dir.up]))
+        {
+            anotherScript.Move((int)Dir.up);
+        }
+        if (Input.GetKey(keys[(int)Dir.down]))
+        {
+            anotherScript.Move((int)Dir.down);
+        }
+        if (Input.GetKey(keys[(int)Dir.left]))
+        {
+            anotherScript.Move((int)Dir.left);
+        }
+        if (Input.GetKey(keys[(int)Dir.right]))
+        {
+            anotherScript.Move((int)Dir.right);
+        }
+    }
+
+
+}
---
+++ UnityProject/Assets/Scripts/Shailesh/Player/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(int dir)
+    {
+        if(dir == 0)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime);
+        }
+        if (dir == 1)
+        {
+            transform.Translate(Vector2.down * Time.deltaTime);
+        }
+        if (dir == 2)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        if (dir == 3)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+    }
+}
