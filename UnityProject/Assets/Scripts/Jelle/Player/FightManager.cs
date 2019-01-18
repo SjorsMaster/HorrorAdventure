28d26042e36959218b73e675fc630dd156d0805b
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2019-01-18 11:21:01 +0100

    Created BeatManager and FightSystem.

---
+++ UnityProject/Assets/Scripts/Jelle/Player/FightManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour
+{   [SerializeField]
+    readonly float detectionRange = 2;
+    readonly string tagName = "Monster";
+    GameObject[] monsterArray;
+    float distanceToMonsterX;
+    float distanceToMonsterY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterArray = GameObject.FindGameObjectsWithTag(tagName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForMonsters();
+    }
+
+    void CheckForMonsters()
+    {
+        monsterArray = GameObject.FindGameObjectsWithTag(tagName);
+        for (int i = 0; i < monsterArray.Length; i++)
+        {
+            distanceToMonsterX =  monsterArray[i].transform.position.x - transform.position.x;
+            distanceToMonsterY =  monsterArray[i].transform.position.y - transform.position.y;
+            if (distanceToMonsterX < detectionRange && distanceToMonsterY < detectionRange)
+            {
+                Debug.Log("Kanker, er is een monster in de buurt");
+            }
+        }
+    }
+}
