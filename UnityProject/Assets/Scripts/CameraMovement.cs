cf63b98bf471beba90d37ac89bde0b0bb4c4e501
Sjors Kaagman <sjorspnk@gmail.com>
2019-01-21 09:43:03 +0100

    Base script

---
+++ UnityProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    GameObject Target;
+    float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
