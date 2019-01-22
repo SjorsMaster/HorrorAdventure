517057c2c44882a87ad8577b14d0ed886a4a7a30
ShaileshSSC <shaileshchotoe2001@gmail.com>
2019-01-22 14:12:20 +0100

    enemy niet vergeten in inputmanager

--- UnityProject/Assets/Scripts/BPM.cs
+++ UnityProject/Assets/Scripts/BPM.cs
@@ -1,33 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BPM : MonoBehaviour
 {
+    [SerializeField]
+    Image Syncer;
+    AudioSource song;
+    [SerializeField]
+    float Offset, bpm = 1;
     float Timer;
+
+    int buttonToPress = -1;
+
     [SerializeField]
     bool Beat;
 
     void Start()
-    {
-        
+    {        
+        song = GameObject.Find("BPMSync").GetComponent<AudioSource>();
+        Syncer = GameObject.Find("BPMSync").GetComponent<Image>();
+        song.Play();
+        Timer = Offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Timer += Time.smoothDeltaTime;
+        Timer += (Time.smoothDeltaTime * bpm);
         if(Timer >= 1)
         {
             Timer = 0;
         }
-        if (Timer <= .1f || Timer >= .9f)
+        if (Timer <= .1f || Timer >= 0.9f)
         {
+            RandomInput();
+            Syncer.enabled = true;
             Beat = true;
         }
         else
         {
+            buttonToPress = -1;
+            Syncer.enabled = false;
             Beat = false;
+        }
+    }
+
+    public void Beatpress(string buttonPressed)
+    {
+        if(int.Parse(buttonPressed) != (buttonToPress + 5))
+        {
+            Debug.Log("DOOD");
+        }
+        else
+        {
+            Debug.Log("win");
+        }
+    }
+
+    void RandomInput()
+    {
+        if(buttonToPress == -1)
+        {
+            buttonToPress = Random.Range(0, 3);
+            Debug.Log(buttonToPress);
         }
     }
 }
