beb786bd123f4e771e5afce9d9785fbdaf6036df
ShaileshSSC <shaileshchotoe2001@gmail.com>
2019-01-28 13:25:39 +0100

    spookjeswoesh kan zweven

---
+++ UnityProject/Assets/Scripts/Shailesh/Enemy/EnemyWosh.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWosh : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float timer = 0;
+    int dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = Random.Range(0,4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 2)
+        {
+            timer = 0;
+            dir = Random.Range(1,5);
+            Debug.Log(dir);
+        }
+        MoveEnemy(dir);
+    }
+
+    void MoveEnemy(int dir)
+    {
+        switch (dir)
+        {
+            case 1:
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+                break;
+            case 2:
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+                break;
+            case 3:
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                break;
+            case 4:
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                break;
+        }
+    }
+
+    public void ChangeSpeed(float speedVar)
+    {
+        speed = speedVar;
+    }
+}
