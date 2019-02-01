512577b50a4e9cd39fcda4e7197e83b87a1e5eb5
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2019-02-01 14:57:26 +0100

    no message

---
+++ UnityProject/Assets/Scripts/Jelle/Camera/FadeOut.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{   [SerializeField]
+    RawImage blackPlane;
+    float minTransparency = 0;
+    float maxTransparency = 1;
+    bool fadeOut = false;
+    float currentTransparency = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackPlane = GetComponent<RawImage>();
+        blackPlane.color = new Color(0, 0, 0, currentTransparency);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeOut && currentTransparency < maxTransparency)
+        {
+            currentTransparency += 0.01f;
+            blackPlane.color = new Color(0, 0, 0, currentTransparency);
+        }
+
+        if (!fadeOut && currentTransparency > minTransparency)
+        {
+            currentTransparency -= 0.01f;
+            blackPlane.color = new Color(0, 0, 0, currentTransparency);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ToggleFadeOut();
+        }
+    }
+
+    void ToggleFadeOut()
+    {
+        fadeOut = !fadeOut;
+    }
+
+}
