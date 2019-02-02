9feef6e469260164eb800a7cff3e0735ba27192f
Sjors Kaagman <sjorspnk@gmail.com>
2019-02-02 12:44:50 +0100

    Quick Final Cleanup, (Moved loose stuff, much more oganized.) also removed hitler's head. (Sorry Jelle, I'm afraid they wouldn't like a spinning head around the player lol.)

---
+++ UnityProject/aardappel/Assets/addforce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforce : MonoBehaviour
+{
+
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            rb.AddForce(Vector3.forward);
+
+
+        if (Input.GetKey("space"))
+            rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+
+        if (Input.GetKey("s"))
+            rb.AddForce(Mathf.Sin(Time.deltaTime), Mathf.Sin(Time.deltaTime), Mathf.Sin(Time.deltaTime), ForceMode.Impulse);
+    }
+}
